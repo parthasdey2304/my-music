110a559e5a4f5b55929e44eea79611dfc38e586e
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-07-25 06:03:22 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import Sidebar from './Components/Sidebar'
-import Mainpage from './Components/Mainpage';
-import Controlspage from './Components/Controlspage';
+// import Sidebar from './Components/Sidebar'
+// import Mainpage from './Components/Mainpage';
+// import Controlspage from './Components/Controlspage';
 
 function App() {
   return (
     <div className="App flex">
       <div className='p-[20px] h-screen w-[20%] bg-white/20'>
-        
+        <div className='flex-col p-4'>
+          <div className='flex'>
+            <i class="ri-music-2-fill text-white text-8xl"></i>
+            <p className='text-5xl font-semibold p-4'>My Music</p>
+          </div>
+
+          <div className="flex pt-[100px]">
+            <p className='text-white/60 font-bold text-xl'>MENU</p>
+          </div>
+        </div>
       </div>
 
       <div className='p-[20px] h-screen w-[60%] border'>Mainpage</div>
